657d0e2a87b255ebc2f3761b2b72ce61e9d225c2
danil <danillatency@gmail.com>
2025-06-28 00:26:51 +0300

    fix: raise exception on unsuccessful linting

--- backend/testing/linter.py
+++ backend/testing/linter.py
@@ -4,8 +4,12 @@ import os
 import pathlib
 
 
+was = False
 for root, files in map(lambda elem: [elem[0], list(filter(lambda el: re.fullmatch("[A-Za-z]+_?[A-Za-z]*.py", el),
                                                           elem[2]))], os.walk(pathlib.Path(__file__).parent.parent
                                                                               / "src")):
     for file in files:
+        was = True
         pycodestyle.Checker(pathlib.Path(root) / file, max_line_length=120).check_all()
+if was:
+    raise SyntaxError("pycodestyle linter found errors!")
