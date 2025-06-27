704a6e4f630be15b60c6267c484ad6f6048cfd9f
danil <danillatency@gmail.com>
2025-06-28 01:25:11 +0300

    fix: regular expression

--- backend/testing/linter.py
+++ backend/testing/linter.py
@@ -5,9 +5,9 @@ import pathlib
 
 
 was = False
-for root, files in map(lambda elem: [elem[0], list(filter(lambda el: re.fullmatch("[A-Za-z]+_?[A-Za-z]*.py", el),
-                                                          elem[2]))], os.walk(pathlib.Path(__file__).parent.parent
-                                                                              / "src")):
+for root, files in map(lambda elem: [elem[0], list(filter(lambda el: re.fullmatch(
+        "[A-Za-z]+(_[A-Za-z]|[A-Za-z])*.py", el),
+                elem[2]))], os.walk(pathlib.Path(__file__).parent.parent / "src")):
     for file in files:
         if pycodestyle.Checker(pathlib.Path(root) / file, max_line_length=120).check_all():
             was = True
