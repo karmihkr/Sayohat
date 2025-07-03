0e6a4e869eb10922984c33d532b22a3601d6a2ae
Arsen Latipov <a.latipov@innopolis.university>
2025-07-03 08:36:11 +0000

    feat #32: edit profile screen

---
+++ frontend/lib/screens/tabs-main-screens/profile-screens/edit_info_screen.dart
@@ -0,0 +1,376 @@
+import 'package:flutter/material.dart';
+import 'package:sayohat/theme/app_colors.dart';
+import 'package:sayohat/user_data.dart';
+import 'package:pattern_formatter/pattern_formatter.dart';
+import 'package:string_validator/string_validator.dart';
+import 'package:sayohat/screens/snack_bar_factory.dart';
+
+bool isValidDate(String input) {
+  if (input == "") {
+    return true;
+  }
+  try {
+    final date = input.split('/');
+    if (date.length != 3) return false;
+
+    final day = int.parse(date[0]);
+    final month = int.parse(date[1]);
+    final year = int.parse(date[2]);
+
+    if (day < 1 || day > 31) return false;
+    if (month < 1 || month > 12) return false;
+    if (year < 1900 || year > DateTime.now().year) return false;
+
+    final DateTime birthDate = DateTime(year, month, day);
+    return birthDate.year == year &&
+        birthDate.month == month &&
+        birthDate.day == day;
+  } catch (e) {
+    return false;
+  }
+}
+
+bool isValidNameSurname(String input) {
+  if (input == "") {
+    return true;
+  }
+  for (int i = 0; i < input.length; i++) {
+    if (isNumeric(input[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool isValidPhone(String input) {
+  if (input == "") {
+    return true;
+  }
+  for (int i = 0; i < input.length; i++) {
+    if (input[i] != "+" && !isNumeric(input[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+String? userName;
+String? userSurname;
+String? userPhone;
+String? userBirth;
+
+class EditInfoScreen extends StatelessWidget {
+  final User user;
+
+  final nameTextController = TextEditingController();
+  final surnameTextController = TextEditingController();
+  final birthTextController = TextEditingController();
+  final phoneTextController = TextEditingController();
+
+  EditInfoScreen({super.key, required this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backgroundBeige,
+      appBar: AppBar(
+        title: Text(
+          'Edit your profile',
+          style: TextStyle(fontFamily: 'Roboto'),
+        ),
+        backgroundColor: AppColors.primaryGreen,
+      ),
+      body: Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Your name: ${user.name}",
+                style: TextStyle(fontFamily: 'Roboto', fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              _NameForm(nameTextController: nameTextController),
+              Divider(),
+              Text(
+                "Your surname: ${user.surname}",
+                style: TextStyle(fontFamily: 'Roboto', fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              _SurnameForm(surnameTextController: surnameTextController),
+              Divider(),
+              Text(
+                "Your phone number: ${user.phone}",
+                style: TextStyle(fontFamily: 'Roboto', fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              _PhoneNumberForm(phoneTextController: phoneTextController),
+              Divider(),
+              Text(
+                "Your date of birth: ${user.birth}",
+                style: TextStyle(fontFamily: 'Roboto', fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              _BirthForm(birthTextController: birthTextController),
+              Spacer(),
+              _ConfirmChanges(
+                newName: nameTextController,
+                newSurname: surnameTextController,
+                newPhone: phoneTextController,
+                newBirth: birthTextController,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _NameForm extends StatelessWidget {
+  final TextEditingController nameTextController;
+
+  const _NameForm({required this.nameTextController});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      height: 40,
+      child: TextField(
+        controller: nameTextController,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          hintText: "New name",
+          filled: true,
+          fillColor: Color.fromRGBO(255, 255, 255, 1),
+        ),
+      ),
+    );
+  }
+}
+
+class _SurnameForm extends StatelessWidget {
+  final TextEditingController surnameTextController;
+
+  const _SurnameForm({required this.surnameTextController});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      height: 40,
+      child: TextField(
+        controller: surnameTextController,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          hintText: "New surname",
+          filled: true,
+          fillColor: Color.fromRGBO(255, 255, 255, 1),
+        ),
+      ),
+    );
+  }
+}
+
+class _PhoneNumberForm extends StatelessWidget {
+  final TextEditingController phoneTextController;
+
+  const _PhoneNumberForm({required this.phoneTextController});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      height: 40,
+      child: TextField(
+        keyboardType: TextInputType.number,
+        controller: phoneTextController,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          hintText: "New phone number",
+          filled: true,
+          fillColor: Color.fromRGBO(255, 255, 255, 1),
+        ),
+      ),
+    );
+  }
+}
+
+class _BirthForm extends StatelessWidget {
+  final TextEditingController birthTextController;
+
+  const _BirthForm({required this.birthTextController});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200.0,
+      height: 40,
+      child: TextField(
+        inputFormatters: [DateInputFormatter()],
+        controller: birthTextController,
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1, color: AppColors.primaryGreen),
+          ),
+          hintText: "New date of birth",
+          filled: true,
+          fillColor: Color.fromRGBO(255, 255, 255, 1),
+        ),
+      ),
+    );
+  }
+}
+
+class _ConfirmChanges extends StatelessWidget {
+  bool? nameValidate;
+  bool? surnameValidate;
+  bool? phoneValidate;
+  bool? birthValidate;
+  TextEditingController newName;
+  TextEditingController newSurname;
+  TextEditingController newPhone;
+  TextEditingController newBirth;
+
+  _ConfirmChanges({
+    required this.newName,
+    required this.newSurname,
+    required this.newPhone,
+    required this.newBirth,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          if (isValidDate(newBirth.text) &&
+              isValidNameSurname(newName.text) &&
+              isValidNameSurname(newSurname.text) &&
+              isValidPhone(newPhone.text)) {
+            user.name = newName.text != "" ? newName.text : user.name;
+            user.surname = newSurname.text != ""
+                ? newSurname.text
+                : user.surname;
+            user.birth = newBirth.text != "" ? newBirth.text : user.birth;
+            user.phone = newPhone.text != "" ? newPhone.text : user.phone;
+            ScaffoldMessenger.of(context).showSnackBar(
+              snackBarFactory.createSnackBar("Info updated successfully"),
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              snackBarFactory.createSnackBar("Something went wrong"),
+            );
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(200, 40),
+          backgroundColor: AppColors.primaryGreen,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+        ),
+        child: const Text(
+          style: TextStyle(
+            color: AppColors.primaryWhite,
+            fontSize: 26.0,
+            fontFamily: 'Roboto',
+          ),
+          "Confirm Changes",
+        ),
+      ),
+    );
+  }
+}
