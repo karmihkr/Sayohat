0251f9ab1bd7decd5bf9e223f140168b0ddfd7ef
danil <danillatency@gmail.com>
2025-07-11 15:07:12 +0300

    fix: unit testing function changed

--- frontend/test/unit_tests/check_vericode_test.dart
+++ frontend/test/unit_tests/check_vericode_test.dart
@@ -4,7 +4,7 @@ import 'package:sayohat/api_client.dart';
 void main() {
   final client = APIClient();
   test('checkVericode returns null for invalid input', () async {
-    final result = await client.checkTelegramVerificationCode('invalid-id', '0000');
-    expect(result == null || result == false, isTrue);
+    final result = await client.obtainAccessToken("+79000000000", 'invalid-id', '0000');
+    expect(result is String, isTrue);
   });
 }
